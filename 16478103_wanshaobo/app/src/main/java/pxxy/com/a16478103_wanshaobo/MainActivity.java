3a92fc5ef335a106a1ac540df68437f3539c2069
Your Name <1647579959@qq.com>
2019-09-06 15:27:19 +0800

    diyizhouzuoye

---
+++ 16478103_wanshaobo/app/src/main/java/pxxy/com/a16478103_wanshaobo/MainActivity.java
@@ -0,0 +1,13 @@
+package pxxy.com.a16478103_wanshaobo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
